efc092b99c4502438cdc4d033949d795695acbf8
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-08-08 19:25:03 -0400

    Tweet can display # of likes/retweet from icon

--- client/src/components/Tweet/ActionBar.js
+++ client/src/components/Tweet/ActionBar.js
@@ -12,16 +12,23 @@ export const ActionBar = (props) => {
       <Action color="rgb(27, 149, 224)" size={40}>
         <TweetActionIcon kind="reply" />
       </Action>
-      <Action color="rgb(23, 191, 99)" size={40}>
-        <TweetActionIcon
-          kind="retweet"
-          //color={isRetweetedByCurrentUser ? "rgb(23, 191, 99)" : undefined}
-          color="rgb(23, 191, 99)"
-        />
-      </Action>
-      <Action color="rgb(224, 36, 94)" size={40}>
-        <LikeButton isLiked={props.isLiked} />
-      </Action>
+
+      <ButtonContainer>
+        <Action color="rgb(23, 191, 99)" size={40}>
+          <TweetActionIcon
+            kind="retweet"
+            //color={isRetweetedByCurrentUser ? "rgb(23, 191, 99)" : undefined}
+            color="rgb(23, 191, 99)"
+          />
+        </Action>
+        {props.numRetweets > 0 && <Count>{props.numRetweets}</Count>}
+      </ButtonContainer>
+      <ButtonContainer>
+        <Action color="rgb(224, 36, 94)" size={40}>
+          <LikeButton isLiked={props.isLiked} />
+        </Action>
+        {props.numLikes > 0 && <Count>{props.numRetweets}</Count>}
+      </ButtonContainer>
       <Action color="rgb(27, 149, 224)" size={40}>
         <TweetActionIcon kind="share" />
       </Action>
@@ -29,8 +36,15 @@ export const ActionBar = (props) => {
   );
 };
 
-const Stat = styled.h1`
-  padding: 5px;
+const Count = styled.h1`
+  padding-left: 5px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* padding: 50px; */
 `;
 
 const Wrapper = styled.div`
